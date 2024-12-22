b1dafa119ffd7d812db8f0dc2ff681410c1352ef
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-12-22 23:56:07 +0500

    java-chat-server

--- Server.java
+++ Server.java
@@ -1,16 +1,78 @@
 public class Server implements Runnable{
+
+    private ArrayList<ConnectionHandler> connections;
+
+
+
+
     @Override
     public void run(){
+        try{
+
+        
         ServerSocket server = new ServerSocket(9999);
+        Socket cline = server.accept();
+        ConnectionHandler handler = new ConnectionHandler(client);
+
+        connections.add(handler);
+        }catch (IOException e){
+            
+            }
 
 
     }
 
+    public void broadcast(String message){
+        for(ConnectionHandler ch :connections){
+            if(ch != null){
+                ch.message(message);
+            }
+
+        }
+    }
+
     class ConnectionHandler implements Runnable{
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+
+        public ConnectionHandler(Socket cline){
+            this.client = client;
+
+        }
 
         @Override
         public void run(){
+            try{
+                out = new PrintWriter(client.getOutPutStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname:");
+                nickname = in.readLine();
+                
+                
+                System.out.println(nickname + " connected!");
+                broadcast(nickname + " joined the chat!")
+                String message;
+                while ((message = in.readLine()) != null){
+                    if(message.startWith("/nick ")){
+                        
+                    }else if(message.startWith("/quit")){
 
+                    }else{
+                        broadcast(nickname+ ": "+ message)
+                    }
+                }
+
+            }catch(IOException){
+
+            }
+
+        }
+
+        public void sendMessage(String message){
+            out.println(message);
         }
     }
 }
